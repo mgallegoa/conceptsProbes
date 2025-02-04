61d74aca81dd23eb526958917e907e01864b027d
mgallegoa <manuelgallego2003@yahoo.com>
2025-02-03 21:05:58 -0500

    ReactAppTodo : Create component for todo and show the mock list.

---
+++ react-ts-todo-app/src/components/Todos.tsx
@@ -0,0 +1,20 @@
+interface Todo {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+type ListOfTodos = Todo[];
+
+interface Props {
+  todos: ListOfTodos;
+}
+
+export const Todos: React.FC<Props> = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+};
