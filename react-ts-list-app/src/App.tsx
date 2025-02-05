82e0409e0ee6edbbeb21a892769491b1a27b8bf3
mgallegoa <manuelgallego2003@yahoo.com>
2025-02-05 11:20:14 -0500

    ReactListApp : Add functionality using recomended read input

--- react-ts-list-app/src/App.tsx
+++ react-ts-list-app/src/App.tsx
@@ -1,10 +1,72 @@
+import { useState } from "react";
 import "./App.css";
 
+interface Item {
+  id: `${string}-${string}-${string}-${string}-${string}`;
+  timestamp: number;
+  text: string;
+}
+
+const INITIAL_ITEMS: Array<Item> = [
+  {
+    id: crypto.randomUUID(),
+    timestamp: Date.now(),
+    text: "Item 1",
+  },
+  {
+    id: crypto.randomUUID(),
+    timestamp: Date.now(),
+    text: "Item 2",
+  },
+];
+
 function App() {
+  const [items, setItems] = useState(INITIAL_ITEMS);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { elements } = event.currentTarget;
+    // Strategy 1: not correct
+    // const input = elements.namedItem('item') as HTMLInputElement
+
+    // Strategy 2: recommended
+    const input = elements.namedItem("item");
+    const isInput = input instanceof HTMLInputElement;
+    if (!isInput || isInput == null) return;
+    const newItem: Item = {
+      id: crypto.randomUUID(),
+      timestamp: Date.now(),
+      text: input.value,
+    };
+    setItems((prevItems) => {
+      return [...prevItems, newItem];
+    });
+    input.value = "";
+  };
   return (
-    <>
-      <h1>List - Add item to list app</h1>
-    </>
+    <main>
+      <aside>
+        <h1>Add Element to a list app</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Element to add:
+            <input
+              name="item"
+              required
+              type="text"
+              placeholder="Write item to add"
+            />
+          </label>
+          <button type="submit">Add Item</button>
+        </form>
+      </aside>
+      <section>
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      </section>
+    </main>
   );
 }
 
