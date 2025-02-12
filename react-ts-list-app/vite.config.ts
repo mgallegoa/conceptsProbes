1c1382784ed0bbfdb44774ea3385cf79f1ecf3b7
mgallegoa <manuelgallego2003@yahoo.com>
2025-02-11 20:42:47 -0500

    ReactListApp : Docker container configuration

--- react-ts-list-app/vite.config.ts
+++ react-ts-list-app/vite.config.ts
@@ -9,4 +9,10 @@ export default defineConfig({
   test: {
     environment: "happy-dom",
   },
+  server: {
+    allowedHosts: [".direct.labs.play-with-docker.com", "localhost"],
+    // host: "0.0.0.0", // Allow external access
+    // port: 5174, // Ensure it's the correct port, the command in package.json do it.
+    // strictPort: true, // Ensures Vite uses this exact port
+  },
 });
