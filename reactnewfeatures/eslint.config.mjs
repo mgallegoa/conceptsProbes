b5d0ecfc916edbd03b4cac7e0f4780aedd0c26d5
mgallegoa <manuelgallego2003@yahoo.com>
2025-02-21 23:21:21 -0500

    Concept Probes : reactnewfeatures upgrade the eslint to version 8.

---
+++ reactnewfeatures/eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+{
+    settings: {
+      react: {
+        version: "detect" // ✅ Automatically detects React version from package.json
+      }
+    },
+    
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
