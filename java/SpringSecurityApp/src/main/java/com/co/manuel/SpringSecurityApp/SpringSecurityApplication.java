04e06209bf76e9331eab6575e89f0171874ad452
mgallegoa <manuelgallego2003@yahoo.com>
2026-03-30 22:24:46 -0500

    Concept Probes : SPRING SECURITY : Basic authentication simulating a user.

--- java/SpringSecurityApp/src/main/java/com/co/manuel/SpringSecurityApp/SpringSecurityApplication.java
+++ java/SpringSecurityApp/src/main/java/com/co/manuel/SpringSecurityApp/SpringSecurityApplication.java
@@ -3,11 +3,12 @@ package com.co.manuel.SpringSecurityApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
 public class SpringSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityApplication.class, args);
+  }
 
 }
---
+++ java/SpringSecurityApp/src/main/java/com/co/manuel/SpringSecurityApp/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.co.manuel.SpringSecurityApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@EnableMethodSecurity: to allow configurations with annotations
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityWebFilterChain(HttpSecurity httpSecurity) {
+    return httpSecurity.build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider(userDetailsService());
+    daoProvider.setPasswordEncoder(passwordEncoder());
+    return daoProvider;
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails userDetails = User.withUsername("manuel")
+        .password("123")
+        .roles("ADMIN")
+        .authorities("READ", "CREATE")
+        .build();
+
+    return new InMemoryUserDetailsManager(userDetails);
+
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    // use BCryptPasswordEncoder() in production;
+    return NoOpPasswordEncoder.getInstance();
+  }
+
+}
